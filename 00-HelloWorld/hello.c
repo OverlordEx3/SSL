9a7d1302f2284ba67b2b29e49f9994c16fba301d
Exequiel Beker <sbeker@frba.utn.edu.ar>
2026-04-26 10:46:02 -0300

    Agrego codigo de ejemplo y configuraciones de lenguaje/compilador

---
+++ 00-HelloWorld/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, wordl!\r\n");
+    printf("Version del lenguaje segun macro __STDC_VERSION__: %ld\r\n", __STDC_VERSION__);
+    return 0;
+}
